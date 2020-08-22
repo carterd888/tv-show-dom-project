2ea444c2eadc439baa3c89aeaf5c85f24c01a643
Daniel Carter <62474051+carterd888@users.noreply.github.com>
2020-08-22 14:14:59 +0100

    completed level 100 features

--- script.js
+++ script.js
@@ -1,12 +1,63 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  // makePageForEpisodes(allEpisodes);
+  getEpisodeData(allEpisodes, generateEpisodeCode);
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
+  let rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
+
+function getEpisodeData(episodeList, generateEpisodeCode) {
+  let rootElem = document.getElementById("root");
+  episodeList.forEach(episode => {
+    let createCard = document.createElement("p");
+    rootElem.appendChild(createCard);
+    createCard.textContent = episode.name;
+
+    let showCode = document.createElement("p");
+    createCard.appendChild(showCode);
+    showCode.textContent = generateEpisodeCode(episode.season, episode.number);
+
+
+    // let seasonNum = document.createElement("p");
+    // createCard.appendChild(seasonNum);
+    // seasonNum.textContent = `Season: ${episode.season}`;
+
+    // let episodeNum = document.createElement("p");
+    // createCard.appendChild(episodeNum);
+    // episodeNum.textContent = `Episode: ${episode.number}`;
+
+    let imgLink = document.createElement("a");
+    imgLink.href = episode.url;
+    imgLink.title = "click for original data";
+    createCard.appendChild(imgLink);
+
+    let imgEl = document.createElement("img");
+    imgEl.src = episode.image.medium;
+    imgLink.appendChild(imgEl);
+
+    let summaryText = document.createElement("p");
+    createCard.appendChild(summaryText);
+    summaryText.innerHTML = episode.summary;
+  });
+  let originalContent = document.createElement("p");
+  rootElem.appendChild(originalContent);
+  originalContent.innerHTML = "Content from TVMaze.com, click image for original data";
+}
+
+function generateEpisodeCode(seasonNum, episodeNum) {
+  if (seasonNum < 10) {
+    seasonNum = "0" + seasonNum;
+  }
+  if (episodeNum < 10) {
+    episodeNum = "0" + episodeNum;
+  }
+  return `S${seasonNum}E${episodeNum}`;
+
+}
+
 
 window.onload = setup;
