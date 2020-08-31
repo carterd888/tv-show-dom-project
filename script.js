53475389083fcfc155bc78582761149b4986205f
Daniel Carter <62474051+carterd888@users.noreply.github.com>
2020-08-31 14:51:30 +0100

    completed level 300

--- script.js
+++ script.js
@@ -6,42 +6,78 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   let rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length}/73 episode(s)`;
+  if(episodeList.length === undefined) {
+    rootElem.textContent = " ";
+  } else {
+    rootElem.textContent = `Got ${episodeList.length}/73 episode(s)`;
+  }
 }
+
+let siteSelect = document.getElementById("siteSelect");
+siteSelect.addEventListener("change", (event) => {
+ let indexEpisode = allEpisodes[siteSelect.value];
+  makePageForEpisodes(indexEpisode); 
+
+  let rootElem = document.getElementById("root");
+  let createCard = document.createElement("p");
+  rootElem.appendChild(createCard);
+  createCard.textContent = indexEpisode.name;
+
+  let showCode = document.createElement("p");
+  createCard.appendChild(showCode);
+  showCode.textContent = generateEpisodeCode(indexEpisode.season, indexEpisode.number);
+
+  let imgLink = document.createElement("a");
+  imgLink.href = indexEpisode.url;
+  imgLink.title = "click for original data";
+  createCard.appendChild(imgLink);
+
+  let imgEl = document.createElement("img");
+  imgEl.src = indexEpisode.image.medium;
+  imgLink.appendChild(imgEl);
+
+  let summaryText = document.createElement("p");
+  createCard.appendChild(summaryText);
+  summaryText.innerHTML = indexEpisode.summary;
+
+  let showAllButton = document.createElement("button");
+  rootElem.appendChild(showAllButton);
+  showAllButton.textContent = "Show all episodes";
+  showAllButton.addEventListener("click", (event) => {
+    setup();
+  })
+})
 
 function getEpisodeData(episodeList, generateEpisodeCode) {
   let rootElem = document.getElementById("root");
-  episodeList.forEach(episode => {
+
+  for(let i = 0; i < episodeList.length; i++) {
+    let option = document.createElement("option");
+    option.value = i;
+    option.textContent = `${generateEpisodeCode(episodeList[i].season, episodeList[i].number)} - ${episodeList[i].name}`;
+    siteSelect.appendChild(option);
+    
     let createCard = document.createElement("p");
     rootElem.appendChild(createCard);
-    createCard.textContent = episode.name;
+    createCard.textContent = episodeList[i].name;
 
     let showCode = document.createElement("p");
     createCard.appendChild(showCode);
-    showCode.textContent = generateEpisodeCode(episode.season, episode.number);
-
-
-    // let seasonNum = document.createElement("p");
-    // createCard.appendChild(seasonNum);
-    // seasonNum.textContent = `Season: ${episode.season}`;
-
-    // let episodeNum = document.createElement("p");
-    // createCard.appendChild(episodeNum);
-    // episodeNum.textContent = `Episode: ${episode.number}`;
+    showCode.textContent = generateEpisodeCode(episodeList[i].season, episodeList[i].number);
 
     let imgLink = document.createElement("a");
-    imgLink.href = episode.url;
+    imgLink.href = episodeList[i].url;
     imgLink.title = "click for original data";
     createCard.appendChild(imgLink);
 
     let imgEl = document.createElement("img");
-    imgEl.src = episode.image.medium;
+    imgEl.src = episodeList[i].image.medium;
     imgLink.appendChild(imgEl);
 
     let summaryText = document.createElement("p");
     createCard.appendChild(summaryText);
-    summaryText.innerHTML = episode.summary;
-  });
+    summaryText.innerHTML = episodeList[i].summary;
+  };
   let originalContent = document.createElement("p");
   rootElem.appendChild(originalContent);
   originalContent.innerHTML = "Content from TVMaze.com, click image for original data";
@@ -57,12 +93,10 @@ function generateEpisodeCode(seasonNum, episodeNum) {
   return `S${seasonNum}E${episodeNum}`;
 }
 
-
   let siteSearch = document.getElementById("siteSearch");
   siteSearch.addEventListener("keyup", (event) => {
     let inputData = event.target.value;
     let filteredSearch = inputData.toUpperCase();
-    console.log(filteredSearch);
     let filteredEpisodes = [];
     allEpisodes.forEach(episode => {
       if (episode.name.toUpperCase().includes(filteredSearch) ||
@@ -72,16 +106,6 @@ function generateEpisodeCode(seasonNum, episodeNum) {
       makePageForEpisodes(filteredEpisodes); 
       getEpisodeData(filteredEpisodes, generateEpisodeCode);
     });
-
-    
     })
-
-  
-  
-
-
-
-
-
 
 window.onload = setup;
