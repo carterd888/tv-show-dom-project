2082aa6291144abd50442abd91207d9742b715f6
carterd888 <62474051+carterd888@users.noreply.github.com>
2020-09-16 20:03:57 +0100

    wip not functioning

--- script.js
+++ script.js
@@ -1,36 +1,89 @@
 // global variable for use in all functions, generates episodes from in file
 // const allEpisodes = getAllEpisodes();
 let allEpisodes;
-let showAllEpisodes;
-let url = "https://api.tvmaze.com/shows/82/episodes";
+let allShows = getAllShows();
+let url;
 
-async function getAllEpis(url) {
-  try {
-  const response = await fetch(url);
-  const data = await response.json();
-  allEpisodes = data;   
+function getAllEpis(url) {
+debugger;
+  fetch(url)
+  .then(
+    response => 
+       response.json())
+       .then(data =>
+        allEpisodes = data
+        )
+    .catch( error => 
+    console.error(error)
+  );  
   // console.log("all episodes are", allEpisodes);
-  } catch (error) {
-    console.error(error);
-  }
+  
 } 
 
 // called on load, with all episode data
-async function setup() {
-  await getAllEpis(url);
-  makePageForEpisodes(allEpisodes);
-  getEpisodeData(allEpisodes, generateEpisodeCode);
+function setup() {
+  displayAllShows(allShows);
+  // await getAllEpis(url);
+  // makePageForEpisodes(allEpisodes);
+  // getEpisodeData(allEpisodes, generateEpisodeCode);
 }
+
+function displayAllShows(episodeList) {
+   let rootElem = document.getElementById("root");
+
+  for(let i = 0; i < episodeList.length; i++) {
+   
+    let createCard = document.createElement("div");
+    createCard.classList.add("card");
+    rootElem.appendChild(createCard);
+    let nameEl = document.createElement("p");
+    createCard.appendChild(nameEl);
+    nameEl.textContent = episodeList[i].name;
+
+    let imgLink = document.createElement("a");
+    imgLink.href = episodeList[i].url;
+    imgLink.title = "click for original data";
+    nameEl.appendChild(imgLink);
+
+    let imgEl = document.createElement("img");
+    if (episodeList[i].image === null) {
+      imgEl.src =
+        "https://tvguide1.cbsistatic.com/www/img/tvg-showcard-placeholder.jpg";
+    } else {
+      imgEl.src = episodeList[i].image.medium;
+    }
+    imgLink.appendChild(imgEl);
+
+    let genres = document.createElement("p");
+    nameEl.appendChild(genres);
+    genres.textContent = `Genres: ${episodeList[i].genres}`;
+
+    let status = document.createElement("p");
+    genres.appendChild(status);
+    status.textContent = `Status: ${episodeList[i].status}`;
+
+    let rating = document.createElement("p");
+    status.appendChild(rating);
+    rating.textContent = `Average Rating: ${episodeList[i].rating.average}`;
+
+    let runtime = document.createElement("p");
+    rating.appendChild(runtime);
+    runtime.textContent = `Runtime: ${episodeList[i].runtime}`;
+
+     let summaryText = document.createElement("p"); // append last
+     runtime.appendChild(summaryText);
+     summaryText.innerHTML = episodeList[i].summary;
+     summaryText.style.fontStyle = "italic";
+     summaryText.style.marginInlineStart = "0px";
+     summaryText.style.marginInlineEnd = "0px";
+}};
+
 
 // shows blank when undefined length (used when selecting from drop down)
 // shows how many episodes are in the list (used for filtering episodes with search)
 function makePageForEpisodes(episodeList) {
   let rootElem = document.getElementById("root");
-  if(episodeList.length === undefined) {
-    rootElem.textContent = " ";
-  } else {
-    rootElem.innerHTML = `Got ${episodeList.length}/${allEpisodes.length}`;
-  }
+  // rootElem.innerHTML = `Got ${episodeList.length}/${allEpisodes.length}`;
 }
 
 // drop down list for episodes makes a page containing episode at specific index
@@ -66,7 +119,12 @@ siteSelect.addEventListener("change", (event) => {
   nameEl.appendChild(imgLink);
 
   let imgEl = document.createElement("img");
-  imgEl.src = indexEpisode.image.medium;
+  if (indexEpisode[i].image === null) {
+    imgEl.src =
+      "https://tvguide1.cbsistatic.com/www/img/tvg-showcard-placeholder.jpg";
+  } else {
+    imgEl.src = indexEpisode[i].image.medium;
+  }
   imgLink.appendChild(imgEl);
 
   let summaryText = document.createElement("p");
@@ -115,7 +173,12 @@ function getEpisodeData(episodeList, generateEpisodeCode) {
     nameEl.appendChild(imgLink);
 
     let imgEl = document.createElement("img");
-    imgEl.src = episodeList[i].image.medium;
+if (episodeList[i].image === null) {
+  imgEl.src =
+    "https://tvguide1.cbsistatic.com/www/img/tvg-showcard-placeholder.jpg";
+} else {
+  imgEl.src = episodeList[i].image.medium;
+}
     imgLink.appendChild(imgEl);
 
     let summaryText = document.createElement("p");
@@ -161,11 +224,20 @@ function generateEpisodeCode(seasonNum, episodeNum) {
     })
 
 let showSelect = document.getElementById("showSelect");
-  let allShows = getAllShows();
-  console.log("all shows are:", allShows);
+  
+//   let allShowsSorted = [];
+//   for (let i = 0; i < allShows.length; i++) {
+
+// allShowsSorted.push({name: allShows[i].name, id: allShows[i].id});
+//   }
+// debugger;
+//   allShowsSorted.sort();
+
+//   console.log("read this", allShowsSorted);
 
 
 for (let i = 0; i < allShows.length; i++) {
+
   let option = document.createElement("option");
   option.value = i;
   option.textContent = `${allShows[i].name}`;
@@ -173,12 +245,99 @@ for (let i = 0; i < allShows.length; i++) {
 }
 showSelect.addEventListener("change", (event) => {
   clearDropdown(siteSelect);
-  let rootElem = document.getElementById("root");
   let show = allShows[showSelect.value];
   url = `https://api.tvmaze.com/shows/${show.id}/episodes`;
-  setup();
+  let rootElem = document.getElementById("root");
+  rootElem.textContent = " ";
+ 
+  console.log(url);
+  getAllEpis(url);
+
+  console.log("all episodes are", allEpisodes);
+  // getEpisodeData(allEpisodes, generateEpisodeCode);
   
 });
+
+
+//WIP CODE
+
+     // let freeSearch = document.getElementById("freeSearch");
+      // freeSearch.addEventListener("keyup", (event) => {
+      //   let inputData = event.target.value;
+      //   let filteredSearch = inputData.toUpperCase();
+      //   let filteredShows = [];
+      //   allShows.forEach((show) => {
+      //     if (
+      //       show.name.toUpperCase().includes(filteredSearch) ||
+      //       show.genres.toUpperCase().includes(filteredSearch) ||
+      //       show.summary.toUpperCase().includes(filteredSearch)
+      //     ) {
+      //       filteredShows.push(show);
+      //     }
+      //      displayAllShows(filteredShows);
+      //   });
+      // });
+
+
+//imgLink.href =
+      // "javascript:getAllEpis(`https://api.tvmaze.com/shows/${episodeList[i].id}/episodes`);";
+    // imgLink.onclick = "getAllEpis(`https://api.tvmaze.com/shows/${episodeList[i].id}/episodes`);";
+
+
+// function displayAllShows(episodeList) {
+//    let rootElem = document.getElementById("root");
+
+//   for(let i = 0; i < episodeList.length; i++) {
+   
+//     let createCard = document.createElement("div");
+//     createCard.classList.add("card");
+//     rootElem.appendChild(createCard);
+//     let nameEl = document.createElement("p");
+//     createCard.appendChild(nameEl);
+//     nameEl.textContent = episodeList[i].name;
+
+//     let imgLink = document.createElement("a");
+//     imgLink.href = episodeList[i].url;
+//     imgLink.title = "click for original data";
+//     nameEl.appendChild(imgLink);
+
+//     let imgEl = document.createElement("img");
+//     imgEl.src = episodeList[i].image.medium;
+//     imgLink.appendChild(imgEl);
+
+//     let genres = document.createElement("p");
+//     nameEl.appendChild(genres);
+//     genres.textContent = `Genres: ${episodeList[i].genres}`;
+
+//     let status = document.createElement("p");
+//     genres.appendChild(status);
+//     status.textContent = `Status: ${episodeList[i].status}`;
+
+//     let rating = document.createElement("p");
+//     status.appendChild(rating);
+//     rating.textContent = `Average Rating: ${episodeList[i].rating.average}`;
+
+//     let runtime = document.createElement("p");
+//     rating.appendChild(runtime);
+//     runtime.textContent = `Runtime: ${episodeList[i].runtime}`;
+
+//      let summaryText = document.createElement("p"); // append last
+//      runtime.appendChild(summaryText);
+//      summaryText.innerHTML = episodeList[i].summary;
+//      summaryText.style.fontStyle = "italic";
+//      summaryText.style.marginInlineStart = "0px";
+//      summaryText.style.marginInlineEnd = "0px";
+// }};
+
+
+
+
+
+
+
+
+
+
 
 
 // calls setup when widows loads    
